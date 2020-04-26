8d5217ba79871ff90145078c45912f6adf9ca4a8
dwianaambarr <dwianaambarr@gmail.com>
2020-04-26 23:58:27 +0700

    First Commit

---
+++ frontend/src/component/CourseApp.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import ListCoursesComponent from './ListCoursesComponent';
+
+
+class CourseApp extends Component{
+  render() {
+  return (
+    <>
+    <h1>Course Application</h1>
+    <ListCoursesComponent/>
+    </> //JSX fragment <>...</>
+  )
+  }
+}
+
+export default CourseApp;
